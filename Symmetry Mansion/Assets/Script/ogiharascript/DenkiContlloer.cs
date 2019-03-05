a7507c44fae2ed2019a7d21494c64f017aba5caa
荻原龍輝 <nkcgta.ogihara.ryuki@gmail.com>
2019-03-05 09:59:17 +0900

    レバー、照明、時計（仮）、エレベーターのスクリプト

---
+++ Symmetry Mansion/Assets/Script/ogiharascript/DenkiContlloer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DenkiContlloer : MonoBehaviour {
+
+    public Image DarkImage;
+
+    public static bool isDengen;
+    public static bool isDengenON;
+    public static bool isDengenOFF;
+
+    float alpha;
+
+    // Use this for initialization
+    void Start()
+    {
+        alpha = 0.0f;
+        isDengen = true;
+        isDengenON = true;
+        isDengenOFF = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDengen)
+        {
+            if (isDengenON) DengenON();
+            if (isDengenOFF) DengenOFF();
+        }
+    }
+    public void DengenON()
+    {
+        alpha = 0.0f;
+        DarkImage.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+        if (alpha <= 0.0f)
+        {
+            isDengen = false;
+            isDengenON = false;
+        }
+    }
+    public void DengenOFF()
+    {
+        alpha = 1.0f;
+        DarkImage.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+        if (alpha >= 1.0f)
+        {
+            isDengen = false;
+            isDengenOFF = false;
+        }
+    }
+}
+
---
+++ Symmetry Mansion/Assets/Script/ogiharascript/clock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class clock : MonoBehaviour {
+
+    public bool sec;   // 秒針の有無
+    public bool secTick;   // 秒針を秒ごとに動かすか
+
+    public GameObject hour;
+    public GameObject minute;
+    public GameObject second;
+
+    void Start()
+    {
+        if (!sec)
+            Destroy(second); // 秒針を消す
+    }
+
+    void Update()
+    {
+        DateTime dt = DateTime.Now;
+
+        hour.transform.eulerAngles = new Vector3(0, 0, (float)dt.Hour / 12 * -360 + (float)dt.Minute / 60 * -30);
+        minute.transform.eulerAngles = new Vector3(0, 0, (float)dt.Minute / 60 * -360);
+        if (sec)
+        {
+            if (secTick)
+                second.transform.eulerAngles = new Vector3(0, 0, (float)dt.Second / 60 * -360);
+            else
+                second.transform.eulerAngles = new Vector3(0, 0, (float)dt.Second / 60 * -360 + (float)dt.Millisecond / 60 / 1000 * -360);
+        }
+
+    }
+}
