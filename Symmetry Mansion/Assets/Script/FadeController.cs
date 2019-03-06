f017051d7fbd114321500af0c4115e883eb30d86
ponta0911 <ponta0911yuto@gmail.cm>
2019-03-06 10:10:44 +0900

    フェードイン・アウトの処理

--- Symmetry Mansion/Assets/Script/FadeController.cs
+++ Symmetry Mansion/Assets/Script/FadeController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FadeController : MonoBehaviour {
 
     public Image FadeImage1;
     public Image FadeImage2;
+
     public static bool isFade1;
     public static bool isFadeOut1;
     public static bool isFadeIn1;
@@ -15,18 +17,29 @@ public class FadeController : MonoBehaviour {
     public static bool isFadeIn2;
     public static float alpha1;
     public static float alpha2;
-	// Use this for initialization
-	void Start () {
-        alpha1 = 1.0f;
-        alpha2 = 1.0f;
+    // Use this for initialization
+    void Start() {
         isFade1 = true;
         isFadeIn1 = true;
         isFadeOut1 = false;
         isFade2 = true;
         isFadeIn2 = true;
         isFadeOut2 = false;
-
-	}
+        //if(SceneManager.GetActiveScene().name== "TitleScene")
+        //{
+        //    alpha1 = 1.0f;
+        //    alpha2 = 0.0f;
+        //}
+        if(SceneManager.GetActiveScene().name=="ClearScene"){
+            alpha1 = 0.0f;
+            alpha2 = 1.0f;
+        }
+        else
+        {
+            alpha1 = 1.0f;
+            alpha2 = 0.0f;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -70,7 +83,7 @@ public class FadeController : MonoBehaviour {
     {
         alpha1 += 0.05f;
         FadeImage1.color = new Color(1.0f, 1.0f, 1.0f, alpha1);
-        if (alpha1 <= 0.0f)
+        if (alpha1 >= 1.0f)
         {
             isFadeOut1 = false;
             isFade1 = false;
@@ -89,12 +102,13 @@ public class FadeController : MonoBehaviour {
 
     public void FadeOut2()
     {
-        alpha2 += 0.05f;
+        alpha2 += 0.02f;
         FadeImage2.color = new Color(1.0f, 1.0f, 1.0f, alpha2);
-        if (alpha2 <= 0.0f)
+        if (alpha2 >= 1.0f)
         {
             isFadeOut2 = false;
             isFade2 = false;
+            SceneManager.LoadScene("ClearScene");
         }
     }
 
