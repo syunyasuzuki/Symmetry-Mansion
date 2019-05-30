0fb56c50d32fe4c09fa18d60aa91d4d562adae42
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2019-05-30 13:36:01 +0900

    タイトルシーン作成中

---
+++ Symmetry Mansion/Assets/Script/Title_Animation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleAnimation : MonoBehaviour
+{
+    Animator animator;
+    public static bool Fade_start;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+        Fade_start = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetTrigger("startTrigger");
+            Invoke("start", 1.5f);
+        }
+	}
+    void start()
+    {
+        Fade_start = true;
+    }
+}
